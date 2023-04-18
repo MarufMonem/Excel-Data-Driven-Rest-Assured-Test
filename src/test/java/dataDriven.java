56ffb92a21dfce94ba714f0542e50e61e590c542
Maruf_Monem <marufmonem@gmail.com>
2023-04-18 14:22:45 +0600

    identifying the testcase column

--- src/test/java/dataDriven.java
+++ src/test/java/dataDriven.java
@@ -1,8 +1,11 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class dataDriven {
     public static void main(String[] args) throws IOException {
@@ -15,6 +18,21 @@ public class dataDriven {
         for(int i=0; i<sheetCount; i++){
             if (workbook.getSheetName(i).equalsIgnoreCase("main")){
                 XSSFSheet tagetSheet= workbook.getSheetAt(i);
+
+                Iterator<Row> rows = tagetSheet.iterator();
+                Row firstRow = rows.next(); // comes to the first row
+
+                Iterator<Cell> cells = firstRow.cellIterator();
+                int count =0;
+                int columnNumber = 0;
+                while (cells.hasNext()){
+                    Cell cellValue = cells.next();
+                    count++;
+                    if(cellValue.getStringCellValue().equalsIgnoreCase("Testcases")){
+                        System.out.println("target cell number: " + count ) ;
+                        columnNumber=count;
+                    }
+                }
             }
         }
 
